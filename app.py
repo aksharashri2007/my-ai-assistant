62c4d98702f41ea985e3b09ae496cc7154171639
aksharashri2007 <aksharashri2007@gmail.com>
2026-09-18 23:07:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,6 @@ def ask():
 
     return jsonify({"answer": response.output_text})
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
